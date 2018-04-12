ee02549d8ddd101b31b576720ade7add7afd839f
Lauren <laurenekcoker@gmail.com>
2018-04-12 16:03:28 -0500

    Set up CSS files and HTML

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path"),
+    webpack = require("webpack");
+
+module.exports = {
+    entry: {
+        App: "./app/assets/scripts/app.js",
+        Vendor: "./app/assets/scripts/vendor.js"
+    },
+    output: {
+        path: path.resolve(__dirname, "./app/temp/scripts"),
+        filename: "[name].js"
+    },
+    module: {
+        rules: [
+            {
+                loader: "babel-loader",
+                query: {
+                    presets: ["es2015"]
+                },
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
