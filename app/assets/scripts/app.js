31cfc24c354238a5824fd95126741f6d4c11b0e3
Lauren <laurenekcoker@gmail.com>
2018-04-17 15:49:06 -0500

    Added event slider functionality!!

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,13 @@
+// import tns from "tiny-slider";
+
+var slider = tns({
+    container: ".large-hero__content",
+    items: 1,
+    slideBy: 1,
+    autoplay: true,
+    speed: 2000,
+    controlsText: "",
+    controlsContainer: ".large-hero__nav"
+});
+
+console.log("Hello!");
